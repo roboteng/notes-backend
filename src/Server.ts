b90c3b01231055b7a14c1309348a54f8294ae745
Trevor Settles <roboteng@gmail.com>
2021-11-15 10:06:46 -0700

    adjusted cors policy header

--- src/Server.ts
+++ src/Server.ts
@@ -2,13 +2,22 @@ import express from "express";
 import LoginRouter from "./routers/login";
 import RegisterRouter from "./routers/register";
 import cookieParer from "cookie-parser";
+import cors from "cors";
+
 import { Database } from "./database/interface";
 import LogoutRouter from "./routers/logout";
+import session from "express-session";
 
 function makeServer(db: Database<number | string>) {
   const app = express();
   app.use(express.json());
   app.use(cookieParer());
+  app.use(cors());
+  app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }));
 
   app.use("/register", RegisterRouter(db));
   app.use("/login", LoginRouter(db));
